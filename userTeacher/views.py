901815895844b328c9ba9af4cb3cc00fa1ba9e93
ais-tau-2024 <uldanovmansur2024@gmail.com>
2025-02-08 20:57:33 +0500

    update README.md

--- userTeacher/views.py
+++ userTeacher/views.py
@@ -3,8 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from custom_auth.models import TeacherModel
 from .serializers import TeacherModelSerializer
+from rest_framework.permissions import AllowAny
 
 class TeacherDetailView(APIView):
+    permission_classes = [AllowAny]
+    
     def get(self, request, iin):
         """
         Получение информации о преподавателе по IIN.
